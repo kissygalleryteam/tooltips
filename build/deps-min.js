b77e3971f28aa5f995b33dafc16e6a82526c912c
kissyteam <tiehang.lth@taobao.com>
2015-02-12 00:33:31 +0800

    sync doc to gh-pages automatically by kpm

--- build/deps-min.js
+++ build/deps-min.js
@@ -1,1 +1,1 @@
-KISSY.config("requires",{"kg/tooltips/1.0.0/index":["node","base","event"],"kg/tooltips/1.0.0/lib/main":["node","base"]});
+KISSY.config("requires",{"kg/tooltips/1.0.1/index":["node","base","event"],"kg/tooltips/1.0.1/lib/main":["node","base"]});
